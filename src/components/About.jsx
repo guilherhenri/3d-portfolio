22c9ac61c7fe3a43c8f8f8c4c82561e2d75dfe1c
guilherhenri <guilherhenri12@gmail.com>
2023-08-13 11:00:37 -0300

    Created Works section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/no-unknown-property */
 import { Tilt } from 'react-tilt'
 import { motion } from 'framer-motion'
 
@@ -9,14 +8,17 @@ import { SectionWrapper } from '../hoc'
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
-    <Tilt className="xs:w-[250px] w-full ">
+    <Tilt
+      className="xs:w-[250px] w-full"
+      options={{ max: 45, scale: 1, speed: 450 }}
+    >
       <motion.div
         variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
         className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
       >
         <div
-          options={{ max: 45, scale: 1, speed: 450 }}
-          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] 
+          flex justify-evenly items-center flex-col"
         >
           <img src={icon} alt={title} className="w-16 h-16 object-contain" />
           <h3 className="text-white text-[20px] font-bold text-center">
